d4bfd9b5a70f3b0dba3355c179c59920a7c615a9
Lauris Bligzna <bligzna.lauris@gmail.com>
2015-03-30 17:01:33 +0300

    fixed js error

--- app/assets/javascripts/views/entry_comments.js
+++ app/assets/javascripts/views/entry_comments.js
@@ -16,13 +16,25 @@ ElixirStream.EntryCommentsView = Ember.View.extend({
         }
       });
     } else {
-      var disqus_shortname = 'elixirstream';
-    
-      (function() {
-        var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-        dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
-        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
-      })();
+      window.disqus_shortname = 'elixirstream';
+      window.disqus_identifier = id;
+      window.disqus_url = window.location.toString();
+      window.disqus_title = title;
+      var dsq = document.createElement('script');
+      dsq.type = 'text/javascript';
+      dsq.async = true;
+      dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+      (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
     }
   }
 });
+
+
+
+// var disqus_shortname = 'elixirstream';
+
+// (function() {
+//     var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+//     dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+//     (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+// })();
