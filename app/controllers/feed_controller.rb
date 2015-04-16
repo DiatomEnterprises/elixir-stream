5b1c075aa1bb76f2418cfeea8b9c295365bd47bc
Lauris Bligzna <bligzna.lauris@gmail.com>
2015-04-16 14:46:47 +0300

    changed order for Entries

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -2,7 +2,7 @@ class FeedController < ApplicationController
   respond_to :rss
 
   def index
-    @entries = Entry.all
+    @entries = Entry.order("created_at DESC")
 
     respond_with @entries
   end
